3e72066768f27458bac8a76285b826d9a7d3502e
Jorge Alexander Valencia Valencia <jorgealexandervalencia@gmail.com>
2018-06-09 14:17:18 -0500

    Add project files.

---
+++ Urna/Form1.Designer.cs
@@ -0,0 +1,280 @@
+﻿namespace Urna
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.rdbSI = new System.Windows.Forms.RadioButton();
+            this.rdbNO = new System.Windows.Forms.RadioButton();
+            this.rdbNSNR = new System.Windows.Forms.RadioButton();
+            this.lblNumVotos = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.progressBar2 = new System.Windows.Forms.ProgressBar();
+            this.progressBar3 = new System.Windows.Forms.ProgressBar();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // rdbSI
+            // 
+            this.rdbSI.AutoSize = true;
+            this.rdbSI.Location = new System.Drawing.Point(13, 22);
+            this.rdbSI.Name = "rdbSI";
+            this.rdbSI.Size = new System.Drawing.Size(35, 17);
+            this.rdbSI.TabIndex = 0;
+            this.rdbSI.TabStop = true;
+            this.rdbSI.Text = "SI";
+            this.rdbSI.UseVisualStyleBackColor = true;
+            // 
+            // rdbNO
+            // 
+            this.rdbNO.AutoSize = true;
+            this.rdbNO.Location = new System.Drawing.Point(12, 45);
+            this.rdbNO.Name = "rdbNO";
+            this.rdbNO.Size = new System.Drawing.Size(41, 17);
+            this.rdbNO.TabIndex = 1;
+            this.rdbNO.TabStop = true;
+            this.rdbNO.Text = "NO";
+            this.rdbNO.UseVisualStyleBackColor = true;
+            // 
+            // rdbNSNR
+            // 
+            this.rdbNSNR.AutoSize = true;
+            this.rdbNSNR.Location = new System.Drawing.Point(12, 68);
+            this.rdbNSNR.Name = "rdbNSNR";
+            this.rdbNSNR.Size = new System.Drawing.Size(104, 30);
+            this.rdbNSNR.TabIndex = 2;
+            this.rdbNSNR.TabStop = true;
+            this.rdbNSNR.Text = "NO SABE/\r\nNO RESPONDE";
+            this.rdbNSNR.UseVisualStyleBackColor = true;
+            // 
+            // lblNumVotos
+            // 
+            this.lblNumVotos.AutoSize = true;
+            this.lblNumVotos.Location = new System.Drawing.Point(104, 26);
+            this.lblNumVotos.Name = "lblNumVotos";
+            this.lblNumVotos.Size = new System.Drawing.Size(64, 13);
+            this.lblNumVotos.TabIndex = 3;
+            this.lblNumVotos.Text = "N° de Votos";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Enabled = false;
+            this.textBox1.Location = new System.Drawing.Point(174, 22);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(298, 107);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(21, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "0%";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(298, 136);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(21, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "0%";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(298, 167);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(21, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "0%";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(174, 97);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(100, 23);
+            this.progressBar1.TabIndex = 8;
+            // 
+            // progressBar2
+            // 
+            this.progressBar2.Location = new System.Drawing.Point(174, 126);
+            this.progressBar2.Name = "progressBar2";
+            this.progressBar2.Size = new System.Drawing.Size(100, 23);
+            this.progressBar2.TabIndex = 9;
+            // 
+            // progressBar3
+            // 
+            this.progressBar3.Location = new System.Drawing.Point(174, 157);
+            this.progressBar3.Name = "progressBar3";
+            this.progressBar3.Size = new System.Drawing.Size(100, 23);
+            this.progressBar3.TabIndex = 10;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(9, 107);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(17, 13);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "SI";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(10, 136);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(23, 13);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "NO";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(9, 167);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(86, 39);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "NO SABE/\r\nNO RESPONDE\r\n\r\n";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 202);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 23);
+            this.button1.TabIndex = 14;
+            this.button1.Text = "Votar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(174, 202);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(100, 23);
+            this.button2.TabIndex = 17;
+            this.button2.Text = "Salir";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Enabled = false;
+            this.textBox2.Location = new System.Drawing.Point(107, 100);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(61, 20);
+            this.textBox2.TabIndex = 18;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Enabled = false;
+            this.textBox3.Location = new System.Drawing.Point(107, 130);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ReadOnly = true;
+            this.textBox3.Size = new System.Drawing.Size(61, 20);
+            this.textBox3.TabIndex = 19;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Enabled = false;
+            this.textBox4.Location = new System.Drawing.Point(107, 164);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.ReadOnly = true;
+            this.textBox4.Size = new System.Drawing.Size(61, 20);
+            this.textBox4.TabIndex = 20;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(342, 255);
+            this.ControlBox = false;
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.progressBar3);
+            this.Controls.Add(this.progressBar2);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.lblNumVotos);
+            this.Controls.Add(this.rdbNSNR);
+            this.Controls.Add(this.rdbNO);
+            this.Controls.Add(this.rdbSI);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Urna ¿Es c# Tu Lenguaje de preferencia?";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RadioButton rdbSI;
+        private System.Windows.Forms.RadioButton rdbNO;
+        private System.Windows.Forms.RadioButton rdbNSNR;
+        private System.Windows.Forms.Label lblNumVotos;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.ProgressBar progressBar2;
+        private System.Windows.Forms.ProgressBar progressBar3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+    }
+}
+
---
+++ Urna/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Urna
+{
+    /// <summary>
+    /// Urna con 3 preguntas (Si,No,NSNR)
+    /// jorge alexander valencia valencia
+    /// </summary>
+    public partial class Form1 : Form
+    {
+        #region Variables
+        int votos = 0; // almacena la cantidad de votos
+        float si = 0.00f; // almacena la respuesta si
+        float no = 0.00f; // almacena la respuesta no
+        float nsnr = 0.00f; // almacena la respuesta no sabe/no responde
+#endregion
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            votos++;            
+            textBox1.Text = String.Format("{0:D}", votos);
+
+            if (rdbSI.Checked) si++;
+            if (rdbNO.Checked) no++;               
+            if (rdbNSNR.Checked) nsnr++;     
+             
+            progressBar1.Value = Convert.ToInt32((si / votos) * 100);
+            progressBar2.Value = Convert.ToInt32((no / votos) * 100);
+            progressBar3.Value = Convert.ToInt32((nsnr / votos) * 100);
+            label1.Text = String.Format("{0:P2}.", (si / votos)); 
+            label2.Text = String.Format("{0:P2}.", (no / votos)); 
+            label3.Text = String.Format("{0:P2}.", (nsnr / votos)); 
+            textBox2.Text = si.ToString();
+            textBox3.Text = no.ToString();
+            textBox4.Text = nsnr.ToString();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
